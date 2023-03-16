c8b2856a63c978050351e17af84b90cba7928015
Javier Evans <j.evans@f5.com>
2023-03-16 14:46:37 -0700

    instruqt challenge 3 start point

--- app/tracing.mjs
+++ app/tracing.mjs
@@ -1,8 +1,16 @@
 import opentelemetry from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: "messenger",
+});
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  resource,
+  traceExporter: new OTLPTraceExporter({ headers: {} }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
