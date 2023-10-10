d102d2721755127fad31491351c63b7cfe708a17
Javier Evans <j.evans@f5.com>
2023-10-09 17:08:56 -0700

    initial cut of a basic demo with instructions

--- app/tracing.mjs
+++ app/tracing.mjs
@@ -1,9 +1,13 @@
+//1
 import opentelemetry from "@opentelemetry/sdk-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 
+//2
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({ headers: {} }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
+//3
 sdk.start();
